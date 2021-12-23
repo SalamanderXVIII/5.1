0d6e9ee5c619e7c8b974752397026b6596c552fd
Андрей Гребенюк <andreygrebenyuk04@gmail.com>
2021-12-23 18:44:57 +0200

    Добавьте файлы проекта.

---
+++ 5.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5.1, 2-й вариант, высокий уровень, Гребенюк А. 21-ИСП-2.
+            Console.WriteLine("Введите число: ");
+            double x = double.Parse(Console.ReadLine());
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Введите n: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(x + ", возведенное в n = " + Power(x, n));
+            }
+        }
+        static double Power(double x, int n)
+        {
+            if (n == 1)
+                return (x);
+            return x * Power(x, n - 1);
+        }
+    }
+}
